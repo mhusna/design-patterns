ecaea50df6d9771bd389b04f0c3a25e5e09eadb8
mhusna <mehmethusna@outlook.com>
2023-06-20 19:56:43 +0300

    Prototype tasarım deseni örnek kod çalışması.

---
+++ prototype-pattern/prototype-demo/prototype-demo/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype_demo
+{
+    public class Customer : Person
+    {
+        public string City { get; set; }
+        public override Person Clone()
+        {
+            return (Person) MemberwiseClone();
+        }
+    }
+}
---
+++ prototype-pattern/prototype-demo/prototype-demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                FirstName = "Mehmet Husna",
+                LastName = "Kisla",
+                City = "İstanbul"
+            };
+            Console.WriteLine(customer1.FirstName);
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Ahmet";
+
+            Console.WriteLine("Customer 1: "+customer1.FirstName +" "+ customer1.LastName);
+            Console.WriteLine("Customer 2: " + customer2.FirstName +" "+ customer2.LastName);
+
+            Console.ReadLine();
+        }
+    }
+}
