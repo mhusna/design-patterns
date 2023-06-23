0740a8c4ad2d4f63ed9a80697aaafd8204fd0ea1
mhusna <mehmethusna@outlook.com>
2023-06-23 21:50:47 +0300

    Template tasarım deseni örnek kod çalışması.

---
+++ memento-pattern/template-demo/template-demo/MensScoringAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_demo
+{
+    public class MensScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+    }
+}
---
+++ memento-pattern/template-demo/template-demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgorithm algorithm
+                ;
+            Console.WriteLine("Mens");
+            algorithm = new MensScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10,new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Women");
+            algorithm = new WomanScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Children");
+            algorithm = new ChildrenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.ReadLine();
+        }
+    }
+}
