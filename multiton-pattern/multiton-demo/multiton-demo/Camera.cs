c923eb49d47c1fa7cc9bd51fda6352e4e447f151
mhusna <mehmethusna@outlook.com>
2023-06-24 12:41:00 +0300

    Multiton tasarım deseni örnek kod çalışması.

---
+++ multiton-pattern/multiton-demo/multiton-demo/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiton_demo
+{
+    public class Camera
+    {
+        static Dictionary<string,Camera> _cameras = new Dictionary<string, Camera>();
+        static object _lock = new object();
+        public Guid Id { get; set; }
+
+        private Camera()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if(!_cameras.ContainsKey(brand))
+                {
+                    _cameras.Add(brand, new Camera());
+                }
+                return _cameras[brand];
+            }
+        }
+    }
+}
---
+++ multiton-pattern/multiton-demo/multiton-demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiton_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera camera1 = Camera.GetCamera("NIKON");
+            Camera camera2 = Camera.GetCamera("CANON");
+            Camera camera3 = Camera.GetCamera("NIKON");
+            Camera camera4 = Camera.GetCamera("CANON");
+
+            Console.WriteLine(camera1.Id);
+            Console.WriteLine(camera2.Id);
+            Console.WriteLine(camera3.Id);
+            Console.WriteLine(camera4.Id);
+
+            Console.ReadLine();
+        }
+    }
+}
